c0350408f693ab82246155fdc21be7827dd931f5
Gabriel Alves <gabrielhas2001@gmail.com>
2023-12-06 12:59:08 -0300

    first commit

---
+++ Introduction/02.Primitive data types.cs
@@ -0,0 +1,10 @@
+Console.WriteLine('b'); // char
+Console.WriteLine(123); // int
+Console.WriteLine(0.25F); // float
+Console.WriteLine(2.625); // double
+Console.WriteLine(12.39816m); // decimal
+
+// boolean
+Console.WriteLine(true); 
+Console.WriteLine(false); 
+
---
+++ Introduction/03.Store data in variables.cs
@@ -0,0 +1,6 @@
+string firstName = "Bob";
+int numberOfMessages = 3;
+double temperature = 34.4F;
+
+Console.WriteLine("Hello, " + firstName + "! You have " + numberOfMessages + " messages in your inbox. The temperature is " + temperature + " celsius.");
+
