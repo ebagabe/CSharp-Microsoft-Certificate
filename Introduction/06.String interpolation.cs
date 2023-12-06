514305aed77d9220e484ebe42a6d74df4ad42c7d
Gabriel Alves <gabrielhas2001@gmail.com>
2023-12-06 15:45:31 -0300

    String conversion

---
+++ Introduction/06.String interpolation.cs
@@ -0,0 +1,13 @@
+string firstName = "Bob";
+string message = $"Hello {firstName}";
+Console.WriteLine(message);
+
+Console.WriteLine("---------------");
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+Console.WriteLine("---------------");
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
