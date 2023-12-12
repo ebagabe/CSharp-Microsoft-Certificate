e12724c109928e5bd0ab39fabc15afbdb455041d
Gabriel Alves <gabrielhas2001@gmail.com>
2023-12-12 19:59:44 -0300

    CodeBlock Course

---
+++ Logic in Console Applications/CodeBlocks/CodeBlocks/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+/*string name = "steve";
+if (name == "bob") Console.WriteLine("Found Bob");
+else if (name == "steve") Console.WriteLine("Found Steve");
+else Console.WriteLine("Found Chuck");
+*/
+
+int[] numbers = {4, 8, 15,16,23,42};
+int total = 0;
+bool found = false;
+
+foreach (var number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+    {
+        found = true;
+    }
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
