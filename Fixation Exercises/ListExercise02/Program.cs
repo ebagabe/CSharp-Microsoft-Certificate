ed02a18b19ae5651c09e7ccdd61bf76d6d9c3d2e
Gabriel Alves <gabrielhas2001@gmail.com>
2023-12-14 19:02:49 -0300

    More about List and matrices

--- Fixation Exercises/ListExercise02/Program.cs
+++ Fixation Exercises/ListExercise02/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+string[] linesAndColumns = Console.ReadLine().Split(' ');
 
-Console.WriteLine("Hello, World!");
+int lines = int.Parse(linesAndColumns[0]);
+int columns = int.Parse(linesAndColumns[1]);
+
+int[,] mat = new int[lines, columns];
+
+for (int i = 0; i < lines; i++)
+{
+    string[] values = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j < columns; j++)
+    {
+        mat[i, j] = int.Parse(values[j]);
+    }
+}
+
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < lines; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        if (mat[i, j] == x)
+        {
+            Console.WriteLine($"Position {i}, {j}:");
+
+            if (j > 0)
+            {
+                Console.WriteLine($"Left: {mat[i,j - 1]}");
+            }
+
+            if (i > 0)
+            {
+                Console.WriteLine($"Up: {mat[i - 1, j]}");
+            }
+
+            if (j < columns - 1)
+            {
+                Console.WriteLine($"Right: {mat[i, j + 1]}");
+            }
+
+            if (i < lines - 1)
+            {
+                Console.WriteLine($"Down: {mat[i + 1, j]}");
+            }
+        }
+    }
+}
