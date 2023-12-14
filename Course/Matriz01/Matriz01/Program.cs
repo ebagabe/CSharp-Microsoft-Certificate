ff50d5a4f5fe1f67036fe8da309272eb255ffc49
Gabriel Alves <gabrielhas2001@gmail.com>
2023-12-14 15:49:21 -0300

    About matrices

---
+++ Course/Matriz01/Matriz01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Array is a data structure
+ Matrix is the name given to two-dimensional arrangements
+ - Homogeneous (Data of the same type)
+ - Sorted (Elements accessed through positions)
+ - Allocated at once, In a contiguous block of memory
+
+ Benefits:
+ - Immediate access to elements based on their position
+ 
+ Disadvantages:
+ - Fixed size
+ - Difficulties in performing insertion and deletions
+*/
+
+double[,] mat = new double[2, 3];
+
+Console.WriteLine(mat.Length); // 6 (Total number of elements)
+Console.WriteLine(mat.Rank); // 2 (Number of lines) 
+Console.WriteLine(mat.GetLength(0)); // First dimension (Lines)
+Console.WriteLine(mat.GetLength(1)); // Second dimension (Columns)
+
